33b76080b0fa84b4a77a5e137e068fb1cb4936ed
pwilliams-ck <pwilliams@cloudkey.io>
2024-03-23 02:05:10 -0500

    Add sidebar - desktop.

---
+++ src/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import Link from 'next/link'
+import { useLocalStorage } from 'usehooks-ts'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+
+import { Plus } from 'lucide-react'
+
+import { Accordion } from '@/components/ui/accordion'
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { NavItem, Organization } from './nav-item'
+
+type SidebarProps = {
+  storageKey?: string
+}
+
+export const Sidebar = ({ storageKey = 'sw-sidebar-state' }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  )
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization()
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true
+    }
+  })
+
+  // This changes { "123": true } --> ["123"]. The Accordion component expects string[].
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key)
+      }
+
+      return acc
+    },
+    []
+  )
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id]
+    }))
+  }
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className="font-medium text-sm flex items-center mb-1">
+        <span>Workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant-="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-5 w-5" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion type="multiple" defaultValue={defaultAccordionValue}>
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
+    </>
+  )
+}
