def2a517431543492269315eb1aa8786e2c98008
pwilliams-ck <pwilliams@cloudkey.io>
2024-03-23 11:47:03 -0500

    Add org settings page to sidebar.

--- src/app/(platform)/(dashboard)/layout.tsx
+++ src/app/(platform)/(dashboard)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from './_components/navbar'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full bg-zinc-50">
+    <div className="h-full">
       <Navbar />
 
       {children}
