03c2f18361b51d70b2729eea9f9eaf9ad3c1ab71
pwilliams-ck <pwilliams@cloudkey.io>
2024-04-08 00:23:15 -0500

    Fix typo.

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -4,14 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster
-       classNames: {
-          error: 'bg-rose-500',
-                success: 'text-green-400',
-      warning: 'text-yellow-400',
-      info: 'bg-blue-400',
-        },
-      />
+      <Toaster />
       {children}
     </ClerkProvider>
   );
