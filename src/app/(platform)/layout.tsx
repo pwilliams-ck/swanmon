44f6817717a1bfccfcca5829f9eaed16bf0f5242
pwilliams-ck <pwilliams@cloudkey.io>
2024-04-08 23:26:05 -0500

    Add api route for modal, client component can't do server action.

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -2,20 +2,23 @@ import { Toaster } from '@/components/ui/sonner';
 import { ClerkProvider } from '@clerk/nextjs';
 
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster
-        theme="system"
-        invert
-        visibleToasts={9}
-        richColors
-        closeButton
-        offset={48}
-      />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster
+          theme="system"
+          invert
+          visibleToasts={9}
+          richColors
+          closeButton
+          offset={48}
+        />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
--- src/components/modals/card-modal/index.tsx
+++ src/components/modals/card-modal/index.tsx
@@ -1,16 +1,25 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { useCardModal } from '@/hooks/use-card-modal';
+import { fetcher } from '@/services/fetcher';
+import { CardWithList } from '@/types';
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ['card', id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>Modal</DialogContent>
+      <DialogContent>{cardData?.title}</DialogContent>
     </Dialog>
   );
 };
